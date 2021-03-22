dc81d5265d7947f6426b967cb533b045f40eded0
ImNamitaKumari <80620555+ImNamitaKumari@users.noreply.github.com>
2021-03-22 16:29:28 +0530

    Update Assignment2.py

--- Assignment2/codes/Assignment2.py
+++ Assignment2/codes/Assignment2.py
@@ -1,7 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
-#for latex equations
+#probability by simulations
 v=np.linspace(0,10,100)
 plt.plot(v,np.exp(-v/2)/2)
 plt.xlabel("$x$")
